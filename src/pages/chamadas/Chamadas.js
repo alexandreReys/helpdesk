1610ba75e97e0f74df0618b749540ab784b20ed8
unknown <anr.alexandre@gmail.com>
2020-11-26 21:23:54 -0300

    .

--- src/pages/chamadas/Chamadas.js
+++ src/pages/chamadas/Chamadas.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { FaEdit, FaPhoneVolume, FaStopCircle, FaUndo } from "react-icons/fa";
+import ReactTooltip from "react-tooltip";
+
 // import { history } from "routes/history";
-
 // import { TextInputMask } from "react-web-masked-text";
-
 // import Sweetalert2 from "sweetalert2";
 // import withReactContent from "sweetalert2-react-content";
 
@@ -37,17 +38,20 @@ const TabelaSac = () => {
 
     return (
         <div id="tabela-sac" className="tabela-sac-container">
-            <div className="tabela-sac-header">
+            <div className="tabela-sac-header" style={{width: 1500}}>
                 <div className="tabela-sac-header-text">
                     Help Desk - Controle de Chamados de Suporte Técnico
                 </div>
             </div>
 
-            <table className="table" style={{ marginTop: 5 }}>
+            <table className="table" style={{ marginTop: 5, width: 1500 }}>
                 <thead style={{ fontSize: "0.9rem" }}>
-                    <tr style={{ fontWeight: "bold", backgroundColor: "#4682b4", color: "white", fontSize: "0.8rem" }}>
+                    <tr style={{ fontWeight: "bold", backgroundColor: "#4682b4", color: "white", fontSize: "1rem" }}>
                         <th scope="col">
-                            <div>Data</div>
+                            <div style={{ marginBottom: 12 }}>Ações</div>
+                         </th>
+                        <th scope="col">
+                        <div style={{ marginBottom: 12 }}>Data</div>
                             <div> </div>
                         </th>
                         <th scope="col">
@@ -74,6 +78,8 @@ const TabelaSac = () => {
                         let backColor;
                         let frontColor;
                         let empresaColor;
+                        let actionColor1 = "yellow";
+                        let actionColor2 = "white";
 
                         const emAlmoco = chamada.EmpresaChamadas.toLowerCase() === "almoço" || chamada.EmpresaChamadas.toLowerCase() === "almoco";
 
@@ -86,19 +92,27 @@ const TabelaSac = () => {
                         if (emAlmoco) {
                             backColor = "#607d8b";
                             frontColor = "white";
+                            actionColor1 = "#ffab40";
+                            actionColor2 = "#80d8ff";
                         } else {
                             if (pendente) {
                                 backColor = "#fff59d";
                                 frontColor = "black";
-                            };
+                                actionColor1 = "blue";
+                                actionColor2 = "#bf360c";
+                                };
                             if (atendendo) {
                                 backColor = "#795548";
                                 frontColor = "white";
-                            };
+                                actionColor1 = "yellow";
+                                actionColor2 = "white";
+                                };
                             if (baixado) {
                                 backColor = "#607d8b";
                                 frontColor = "silver";
-                            };
+                                actionColor1 = "#ffab40";
+                                actionColor2 = "#80d8ff";
+                                };
                         };
 
                         if (pendente) {
@@ -118,13 +132,37 @@ const TabelaSac = () => {
                         return (
                             <tr
                                 key={chamada.index}
-                                style={{ backgroundColor: backColor, color: frontColor, fontSize: "0.6rem" }}
+                                style={{ backgroundColor: backColor, color: frontColor, fontSize: "0.8rem" }}
                             >
-                                <td style={{ width: "10%" }}>
+                                <td style={{ width: "9%", minWidth: 80, maxWidth: 90, paddingRight: 0 }}>
+                                    <div style={{ color: "yellow"}}>
+                                        {!baixado &&
+                                            <>
+                                                <FaPhoneVolume data-tip="Atender" style={{ cursor: "pointer", marginRight: 10, color: actionColor1, fontSize: "1.2rem"}}/>
+                                                <ReactTooltip place="bottom" effect="solid" className="tool-tip" /> 
+                                                
+                                                <FaStopCircle data-tip="Baixar" style={{ cursor: "pointer", marginRight: 10, color: actionColor2, fontSize: "1.2rem"}}/> 
+                                                <ReactTooltip place="bottom" effect="solid" className="tool-tip" /> 
+                                            </>
+                                        }
+                                        
+                                        <FaEdit data-tip="Alterar" style={{ cursor: "pointer", marginRight: 10, color: actionColor1, fontSize: "1.2rem"}}/>
+                                        <ReactTooltip place="bottom" effect="solid" className="tool-tip" /> 
+                                        
+                                        {!baixado &&
+                                            <>
+                                                <FaUndo data-tip="Voltar para Pendente" style={{ cursor: "pointer", marginRight: 10, color: actionColor2, fontSize: "1.2rem"}}/>
+                                                <ReactTooltip place="bottom" effect="solid" className="tool-tip" /> 
+                                            </>
+                                        }
+                                    </div>
+                                </td>
+
+                                <td style={{ width: "6%", minWidth: 80, maxWidth: 90, paddingRight: 0 }}>
                                     <div>{utils.formattedDateTimeNoYear(chamada.DataChamadas, chamada.HoraChamadas)}</div>
                                 </td>
 
-                                <td style={{ width: "10%" }}>
+                                <td style={{ width: "6%", minWidth: 80, maxWidth: 90, paddingRight: 0 }}>
                                     <div> {!emAlmoco && chamada.CodEmpresaChamadas} </div>
 
                                     {!baixado &&
@@ -134,7 +172,7 @@ const TabelaSac = () => {
                                     }
                                 </td>
 
-                                <td style={{ width: "10%" }}>
+                                <td style={{ width: "8%", minWidth: 80, maxWidth: 90, paddingRight: 0 }}>
                                     <div style={{ textTransform: "uppercase", fontWeight: "bold", color: emAlmoco? "white": empresaColor }}>
                                         {chamada.AnalistaChamadas}
                                     </div>
@@ -143,14 +181,14 @@ const TabelaSac = () => {
                                     </div>
                                 </td>
 
-                                <td style={{ width: "20%" }}>
+                                <td style={{ width: "14%", minWidth: 80, maxWidth: 90, paddingRight: 0 }}>
                                     <div> {!emAlmoco && chamada.ContatoChamadas} </div>
                                     {!baixado &&
                                         <div> {!emAlmoco && chamada.TelefoneChamadas} </div>
                                     }
                                 </td>
 
-                                <td style={{ width: "50%" }}>
+                                <td style={{ paddingRight: 0 }}>
                                     {!!baixado &&
                                         <div>
                                             { emAlmoco &&
