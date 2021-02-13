15edde98199777ecd845a682540399c088f30ced
alexandrereys <anr.alexandre@gmail.com>
2021-02-13 11:41:23 -0300

    refactor(chamadasService): post/put 'evento'

--- src/services/chamadasService.js
+++ src/services/chamadasService.js
@@ -33,7 +33,6 @@ export const post = async (updateData) => {
 };
 
 export const put = async (updateData) => {
-    console.log("updateData", updateData);
     try {
         var resp = await api.put("/api/v1/suporte/chamadas?idEmpresaChamadas=1", updateData);
     } catch (error) {
@@ -44,6 +43,88 @@ export const put = async (updateData) => {
 };
 
 export const postEvento = async (tipoEvento, updateData) => {
+    const isInsert = tipoEvento === "incluir";
+    const integrarComAnr = store.getState().defaultState.IntegracaoAnrSettings === 1;
+
+    if ( !integrarComAnr ) {
+        ajustaCamposEvento();
+        if( isInsert ) { post(updateData) } else { put(updateData) };
+
+    } else {
+
+        if( !updateData.IdParadoxChamadas && !isInsert ) return false;
+
+        const loggedUser = store.getState().loginState.loggedUser;
+        const data = { ...updateData, LoggedUser: loggedUser };
+        
+        const url = "/api/v1/suporte/eventos?idEmpresaChamadas=1&tipoEvento=";
+        try {
+            await api.post(url + tipoEvento, data);
+        } catch (error) {
+            console.error("ErrorMessage (chamadasService.postEvento): ", error);
+        };
+
+        if( isInsert ) {
+            // Será incluido no mysql pelo integradorParadox.exe
+        } else {
+            ajustaCamposEvento();
+            put(updateData);
+        };
+    };
+
+    function ajustaCamposEvento() {
+        const loggedUser = store.getState().loginState.loggedUser;
+        const info = `. - ${ loggedUser } - ${ utils.getDateNow() } - ${ utils.getTimeNow() }`;
+
+        if (tipoEvento === "incluir") {
+            updateData = { 
+                ...updateData, 
+                SituacaoChamadas: "Pendente", 
+                PrioridadeChamadas: 5, 
+                IncluidoPorChamadas: "Inc" + info,
+            };
+        };
+    
+        if (tipoEvento === "alterar") {
+            updateData = { 
+                ...updateData, 
+                Obs5Chamadas: "Alt" + info,
+            };
+        };
+    
+        if (tipoEvento === "atender") {
+            updateData = { 
+                ...updateData, 
+                SituacaoChamadas: "Atendendo", 
+                PrioridadeChamadas: 7, 
+                AtendidoPorChamadas: "Ate" + info,
+                DataAltChamadas: utils.getDateNowYMD(),
+                HoraAltChamadas: utils.getTimeNow(),
+            };
+        };
+    
+        if (tipoEvento === "baixar") {
+            updateData = { 
+                ...updateData, 
+                SituacaoChamadas: "Baixado", 
+                PrioridadeChamadas: 10, 
+                BaixadoPorChamadas: "Bai" + info,
+            };
+        };
+    
+        if (tipoEvento === "voltar") {
+            updateData = { 
+                ...updateData, 
+                SituacaoChamadas: "Pendente", 
+                PrioridadeChamadas: 1, 
+                Obs5Chamadas: "Nov" + info,
+            };
+        };
+    };
+};
+
+
+export const postEvento2 = async (tipoEvento, updateData) => {
     const isInsert = tipoEvento === "incluir";
     const integrarComAnr = store.getState().defaultState.IntegracaoAnrSettings === 1;
     
@@ -58,7 +139,7 @@ export const postEvento = async (tipoEvento, updateData) => {
             console.error("ErrorMessage (chamadasService.postEvento): ", error);
         };
 
-        if ( isInsert ) return;  // Será incluido no mysql pelo integradorParadox.exe
+        if( isInsert ) return;  // Será incluido no mysql pelo integradorParadox.exe
     };
 
     ajustaCamposEvento();
