16055b2029ec87110844813fad7133deaaecd1b5
alexandrereys <anr.alexandre@gmail.com>
2021-12-08 08:37:12 -0300

    changed default api url to kinghost url

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-
 import * as loginService from "./loginService";
 
-const mysqlBaseUrl =
-  process.env.REACT_APP_BASE_URL || "https://anrsistemas.com";
+
+// const mysqlBaseUrl =
+//   process.env.REACT_APP_BASE_URL || "https://anrsistemas.com";
+
+  const mysqlBaseUrl =
+  process.env.REACT_APP_BASE_URL || "http://testenode.kinghost.net:21090";
 
 export const api = axios.create({
   baseURL: mysqlBaseUrl,
